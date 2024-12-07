f5a9c043928b3f0ca23043c842b8b6d9a17ca86d
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2024-12-07 13:18:37 +0100

    Improve LuaHandler code

--- luahandler.cpp
+++ luahandler.cpp
@@ -2,18 +2,18 @@
 
 #define LUA_LOG "###LUA_LOG :"
 
-LuaHandler::LuaHandler(void) {
+LuaHandler::LuaHandler() {
     this->lua = luaL_newstate();
     luaL_openlibs(lua);
 }
 
-LuaHandler::~LuaHandler(void) {
+LuaHandler::~LuaHandler() {
     if(this->lua) {
         lua_close(lua);
     }
 }
 
-bool LuaHandler::openFile(std::string fileName) {
+bool LuaHandler::openFile(const std::string& fileName) const {
     luaL_loadfile(lua, fileName.c_str());
     if (lua_pcall(lua, 0, 0, 0) != 0) {
         printf("%sError at loading file %s. Message is: %s.\r\n", LUA_LOG, fileName.c_str(), lua_tostring(lua, -1));
@@ -23,167 +23,150 @@ bool LuaHandler::openFile(std::string fileName) {
     return true;
 }
 
-bool LuaHandler::getGlobalBoolean(std::string globalVariableName) {
+bool LuaHandler::getGlobalBoolean(const std::string& globalVariableName) const{
     lua_getglobal(lua, globalVariableName.c_str());
     if(lua_isboolean(lua, -1)) {
-        bool value = lua_toboolean(lua, -1);
+        const bool value = lua_toboolean(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load boolean variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
-        lua_pop(lua, 1);
     }
+    printf("%sUnable to load boolean variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
+    lua_pop(lua, 1);
     return false;
 }
 
-int LuaHandler::getGlobalInteger(std::string globalVariableName) {
+int LuaHandler::getGlobalInteger(const std::string& globalVariableName) const {
     if(lua_getglobal(lua, globalVariableName.c_str()) == LUA_TNUMBER) {
-        int value = static_cast<int>(lua_tonumber(lua, -1));
+        const int value = static_cast<int>(lua_tonumber(lua, -1));
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load int variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
-        lua_pop(lua, 1);
     }
+    printf("%sUnable to load int variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
+    lua_pop(lua, 1);
     return -1;
 }
 
-float LuaHandler::getGlobalNumber(std::string globalVariableName) {
+float LuaHandler::getGlobalNumber(const std::string& globalVariableName) const {
     lua_getglobal(lua, globalVariableName.c_str());
     if(lua_isnumber(lua, -1)) {
-        float value = lua_tonumber(lua, -1);
+        const float value = lua_tonumber(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load float variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
-        lua_pop(lua, 1);
     }
+    printf("%sUnable to load float variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
+    lua_pop(lua, 1);
+
     return -1.0f;
 }
 
-std::string LuaHandler::getGlobalString(std::string globalVariableName) {
+std::string LuaHandler::getGlobalString(const std::string& globalVariableName) const {
     lua_getglobal(lua, globalVariableName.c_str());
     if(lua_isstring(lua, -1)) {
-        std::string value = lua_tostring(lua, -1);
+        const std::string value = lua_tostring(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load string variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
-        lua_pop(lua, 1);
     }
-    return "\0";
+    printf("%sUnable to load string variable named: %s.\r\n", LUA_LOG, globalVariableName.c_str());
+    lua_pop(lua, 1);
+    return "";
 }
 
-bool LuaHandler::getBoolFromTable(std::string key) {
+bool LuaHandler::getBoolFromTable(const std::string& key) const {
     lua_pushstring(lua, key.c_str());
     lua_gettable(lua, -2);
-
     if(lua_isboolean(lua, -1)) {
-        bool value = lua_toboolean(lua, -1);
+        const bool value = lua_toboolean(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load boolean variable with key: %s.\r\n", LUA_LOG, key.c_str());
-        lua_pop(lua, 1);
-        return false;
     }
+    printf("%sUnable to load boolean variable with key: %s.\r\n", LUA_LOG, key.c_str());
+    lua_pop(lua, 1);
+    return false;
 }
 
-int LuaHandler::getIntegerFromTable(std::string key) {
+int LuaHandler::getIntegerFromTable(const std::string& key) const {
     lua_pushstring(lua, key.c_str());
     lua_gettable(lua, -2);
 
     if(lua_isinteger(lua, -1)) {
-        int value = static_cast<int>(lua_tointeger(lua, -1));
+        const int value = static_cast<int>(lua_tointeger(lua, -1));
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load integer variable with key: %s.\r\n", LUA_LOG, key.c_str());
-        lua_pop(lua, 1);
-        return -1;
     }
+    printf("%sUnable to load integer variable with key: %s.\r\n", LUA_LOG, key.c_str());
+    lua_pop(lua, 1);
+    return -1;
 }
 
-int LuaHandler::getIntegerFromTable(int index) {
+int LuaHandler::getIntegerFromTable(int index) const {
     lua_pushinteger(lua, index);
     lua_gettable(lua, -2);
-
     if(lua_isinteger(lua, -1)) {
-        int value = static_cast<int>(lua_tointeger(lua, -1));
+        const int value = static_cast<int>(lua_tointeger(lua, -1));
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load integer variable with index: %d.\r\n", LUA_LOG, index);
-        lua_pop(lua, 1);
-        return -1;
     }
+    printf("%sUnable to load integer variable with index: %d.\r\n", LUA_LOG, index);
+    lua_pop(lua, 1);
+    return -1;
 }
 
-float LuaHandler::getNumberFromTable(int index) {
+float LuaHandler::getNumberFromTable(const int index) const{
     lua_pushinteger(lua, index);
     lua_gettable(lua, -2);
-
     if(lua_isnumber(lua, -1)) {
-        float value = lua_tonumber(lua, -1);
+        const float value = lua_tonumber(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load number variable with index: %d.\r\n", LUA_LOG, index);
-        lua_pop(lua, 1);
-        return -1.0f;
     }
+    printf("%sUnable to load number variable with index: %d.\r\n", LUA_LOG, index);
+    lua_pop(lua, 1);
+    return -1.0f;
 }
 
-float LuaHandler::getNumberFromTable(std::string key) {
+float LuaHandler::getNumberFromTable(const std::string& key) const {
     lua_pushstring(lua, key.c_str());
     lua_gettable(lua, -2);
-
     if(lua_isnumber(lua, -1)) {
-        float value = lua_tonumber(lua, -1);
+        const float value = lua_tonumber(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load number variable with key: %s.\r\n", LUA_LOG, key.c_str());
-        lua_pop(lua, 1);
-        return -1.0f;
     }
+    printf("%sUnable to load number variable with key: %s.\r\n", LUA_LOG, key.c_str());
+    lua_pop(lua, 1);
+    return -1.0f;
 }
 
-std::string LuaHandler::getStringFromTable(int index) {
+std::string LuaHandler::getStringFromTable(const int index) const {
     lua_pushinteger(lua, index);
     lua_gettable(lua, -2);
-
     if(lua_isstring(lua, -1)) {
-        std::string value = lua_tostring(lua, -1); 
+        const std::string value = lua_tostring(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load string variable with index: %d.\r\n", LUA_LOG, index);
-        lua_pop(lua, 1);
-        return "\0";
     }
+    printf("%sUnable to load string variable with index: %d.\r\n", LUA_LOG, index);
+    lua_pop(lua, 1);
+    return "";
 }
 
-std::string LuaHandler::getStringFromTable(std::string key) {
+std::string LuaHandler::getStringFromTable(const std::string& key) const {
     lua_pushstring(lua, key.c_str());
     lua_gettable(lua, -2);
-
     if(lua_isstring(lua, -1)) {
-        std::string value = lua_tostring(lua, -1); 
+        const std::string value = lua_tostring(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to load string variable with key: %s.\r\n", LUA_LOG, key.c_str());
-        lua_pop(lua, 1);
-        return "\0";
     }
+    printf("%sUnable to load string variable with key: %s.\r\n", LUA_LOG, key.c_str());
+    lua_pop(lua, 1);
+    return "";
 }
 
-bool LuaHandler::getTableFromTable(int index) {
+bool LuaHandler::getTableFromTable(const int index) const {
     lua_pushinteger(lua, index);
     lua_gettable(lua, -2);
-
-    bool success = isTopOfStackATable();
+    const bool success = isTopOfStackATable();
     if(!success) {
         printf("%sUnable to read table indexed %d from current table.\r\n", 
         LUA_LOG, index);
@@ -192,11 +175,10 @@ bool LuaHandler::getTableFromTable(int index) {
     return success;
 }
 
-bool LuaHandler::getTableFromTable(std::string key) {
+bool LuaHandler::getTableFromTable(const std::string& key) const {
     lua_pushstring(lua, key.c_str());
     lua_gettable(lua, -2);
-
-    bool success = isTopOfStackATable();
+    const bool success = isTopOfStackATable();
     if(!success) {
         printf("%sUnable to read table named %s from current table.\r\n", 
         LUA_LOG, key.c_str());
@@ -205,18 +187,18 @@ bool LuaHandler::getTableFromTable(std::string key) {
     return success;
 }
 
-bool LuaHandler::isTopOfStackATable(void) const {
+bool LuaHandler::isTopOfStackATable() const {
     return lua_istable(lua, -1);
 }
 
-void LuaHandler::loadTable(std::string tableName) {
+void LuaHandler::loadTable(const std::string& tableName) const {
     lua_getglobal(lua, tableName.c_str());
     if(!isTopOfStackATable()) {
         printf("%sUnable to read table named: %s.\r\n", LUA_LOG, tableName.c_str());
     }
 }
 
-void LuaHandler::popTable(void) {
+void LuaHandler::popTable() const {
     if(isTopOfStackATable()) {
         lua_pop(lua, 1);
     } else {
@@ -224,9 +206,9 @@ void LuaHandler::popTable(void) {
     }
 }
 
-bool LuaHandler::getFunction(std::string functionName) {
+bool LuaHandler::getFunction(const std::string& functionName) const {
     lua_getglobal(lua, functionName.c_str());
-    bool validFunction = lua_isfunction(lua, -1);
+    const bool validFunction = lua_isfunction(lua, -1);
     if(!validFunction) {
         printf("%sAttempt to load function named \"%s\" failed.\r\n", LUA_LOG, functionName.c_str());
         lua_pop(lua, 1);
@@ -234,7 +216,7 @@ bool LuaHandler::getFunction(std::string functionName) {
     return validFunction;
 }
 
-int LuaHandler::callFunctionFromStack(int parameters, int returns) {
+int LuaHandler::callFunctionFromStack(const int parameters, const int returns) const {
     return lua_pcall(lua, parameters, returns, 0);
 }
 
@@ -243,84 +225,79 @@ int LuaHandler::callFunctionFromStack(int parameters, int returns) {
  * Returns the lua_pcall method return.
  * Results will be pushed to the stack.
  */
-int LuaHandler::getAndCallFunction(std::string functionName, int returns) {
+int LuaHandler::getAndCallFunction(const std::string& functionName, const int returns) const {
     lua_getglobal(lua, functionName.c_str());
     return lua_pcall(lua, 0, returns, 0);
 }
 
-void LuaHandler::pushBoolean(bool value) {
+void LuaHandler::pushBoolean(const bool value) const {
     lua_pushboolean(lua, value);
 }
 
-void LuaHandler::pushInteger(int value) {
+void LuaHandler::pushInteger(const int value) const {
     lua_pushinteger(lua, value);
 }
 
-void LuaHandler::pushNumber(float value) {
+void LuaHandler::pushNumber(const float value) const {
     lua_pushnumber(lua, value);
 }
 
-void LuaHandler::pushString(std::string value) {
+void LuaHandler::pushString(const std::string& value) const {
     lua_pushstring(lua, value.c_str());
 }
 
-bool LuaHandler::popBoolean() {
+bool LuaHandler::popBoolean() const {
     if(!lua_isnil(lua, -1)) {
-        bool value = lua_toboolean(lua, -1);
+        const bool value = lua_toboolean(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to pop boolean value from top. Top is nil.\r\n", LUA_LOG);
     }
+    printf("%sUnable to pop boolean value from top. Top is nil.\r\n", LUA_LOG);
     return false;
 }
 
-int LuaHandler::popInteger() {
+int LuaHandler::popInteger() const {
     if(!lua_isnil(lua, -1)) {
-        int value = lua_tointeger(lua, -1);
+        const int value = lua_tointeger(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to pop integer value from top. Top is nil.\r\n", LUA_LOG);
     }
+    printf("%sUnable to pop integer value from top. Top is nil.\r\n", LUA_LOG);
     return -1;
 }
 
-float LuaHandler::popNumber() {
+float LuaHandler::popNumber() const {
     if(!lua_isnil(lua, -1)) {
-        float value = lua_tonumber(lua, -1);
+        const float value = lua_tonumber(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to pop float value from top. Top is nil.\r\n", LUA_LOG);
     }
+    printf("%sUnable to pop float value from top. Top is nil.\r\n", LUA_LOG);
     return -1.0f;
 }
 
-std::string LuaHandler::popString() {
+std::string LuaHandler::popString() const {
     if(!lua_isnil(lua, -1)) {
         std::string value = lua_tostring(lua, -1);
         lua_pop(lua, 1);
         return value;
-    } else {
-        printf("%sUnable to pop string value from top. Top is nil.\r\n", LUA_LOG);
     }
-    return "\0";
+    printf("%sUnable to pop string value from top. Top is nil.\r\n", LUA_LOG);
+    return "";
 }
 
-int LuaHandler::popTop(void) {
+int LuaHandler::popTop() const{
     if(this->getStackTop() > 0) {
         lua_pop(lua, 1);
     }
-
     return this->getStackTop();
 }
 
-int LuaHandler::getStackTop(void) const {
+int LuaHandler::getStackTop() const {
     return lua_gettop(lua);
 }
 
 ///Get the length of the object on top of the stack.
-int LuaHandler::getLength(void) const {
-    return lua_rawlen(lua, -1);
+int LuaHandler::getLength() const {
+    return static_cast<int>(lua_rawlen(lua, -1));
 }
--- luahandler.hpp
+++ luahandler.hpp
@@ -4,54 +4,49 @@
 #include <string>
 
 class LuaHandler {
-    public:
-
-    private:
         lua_State* lua;
-
     public:
-        LuaHandler(void);
-        ~LuaHandler(void);
+        LuaHandler();
+        ~LuaHandler();
 
-    public:
-        bool openFile(std::string fileName);
-        bool getGlobalBoolean(std::string globalVariableName);
-        int getGlobalInteger(std::string globalVariableName);
-        float getGlobalNumber(std::string globalVariableName);
-        std::string getGlobalString(std::string globalVariableName);
+        bool openFile(const std::string& fileName) const;
+        bool getGlobalBoolean(const std::string& globalVariableName) const;
+        int getGlobalInteger(const std::string& globalVariableName) const;
+        float getGlobalNumber(const std::string& globalVariableName) const;
+        std::string getGlobalString(const std::string& globalVariableName) const;
 
-        bool getBoolFromTable(std::string key);
-        int getIntegerFromTable(int index);
-        int getIntegerFromTable(std::string key);
-        float getNumberFromTable(int index);
-        float getNumberFromTable(std::string key);
-        std::string getStringFromTable(int index);
-        std::string getStringFromTable(std::string key);
+        bool getBoolFromTable(const std::string& key) const;
+        int getIntegerFromTable(int index) const;
+        int getIntegerFromTable(const std::string& key) const;
+        float getNumberFromTable(int index) const;
+        float getNumberFromTable(const std::string& key) const;
+        std::string getStringFromTable(int index) const;
+        std::string getStringFromTable(const std::string& key) const;
 
-        bool getTableFromTable(int index);
-        bool getTableFromTable(std::string key);
+        bool getTableFromTable(int index) const;
+        bool getTableFromTable(const std::string& key) const;
         ///Returns true or false if the top of the stack is a table.
-        bool isTopOfStackATable(void) const;
+        bool isTopOfStackATable() const;
 
-        bool getFunction(std::string functionName);
-        int callFunctionFromStack(int parameters, int returns);
-        int getAndCallFunction(std::string functionName, int returns);
+        bool getFunction(const std::string& functionName) const;
+        int callFunctionFromStack(int parameters, int returns) const;
+        int getAndCallFunction(const std::string&, int returns) const;
 
-        void pushBoolean(bool value);
-        void pushInteger(int value);
-        void pushNumber(float value);
-        void pushString(std::string value);
+        void pushBoolean(bool value) const;
+        void pushInteger(int value) const;
+        void pushNumber(float value) const;
+        void pushString(const std::string& value) const;
 
-        bool popBoolean();
-        int popInteger();
-        float popNumber();
-        std::string popString();
+        bool popBoolean() const;
+        int popInteger() const;
+        float popNumber() const;
+        std::string popString() const;
 
-        void loadTable(std::string tableName);
-        void popTable(void);
+        void loadTable(const std::string& tableName) const;
+        void popTable() const;
 
         ///Pop the top of the stack and return the stack top.
-        int popTop(void);
-        int getStackTop(void) const;
-        int getLength(void) const;
+        int popTop() const;
+        int getStackTop() const;
+        int getLength() const;
 };
